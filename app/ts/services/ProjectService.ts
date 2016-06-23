5a6f69d7efe442ad96aec984b2d861eeb364caa0
Chris Lankford <christopherlankford@gmail.com>
2016-06-22 23:43:34 -0700

    Added comments

--- app/ts/services/ProjectService.ts
+++ app/ts/services/ProjectService.ts
@@ -1,3 +1,4 @@
+// Angular
 import { Injectable, provide } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
@@ -5,16 +6,19 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class ProjectService {
+    // URL to backend API
     static BASE_URL: string = 'http://localhost:3000'
     
     constructor(public http: Http) {}
     
+    // API call to get project by ID
     getProject(id: string): Observable<any> {
         let queryURL: string = `${ProjectService.BASE_URL}/project/${id}`;
         return this.http.request(queryURL).map((res: any) => res.json());
     }
 }
 
+// Declare the provider and make it available for DI.
 export var PROJECT_PROVIDERS: Array<any> = [
     provide(ProjectService, {useClass: ProjectService})
 ];
